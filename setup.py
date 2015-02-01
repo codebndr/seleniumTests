b9ef88613388d37d437e31e384edeaac2ffa6e05
Brandon Duffany <bsd26@cornell.edu>
2015-02-01 13:36:00 -0500

    Add basic configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='codebender_selenium',
+    version='0.0.0',
+    description='Selenium tests for codebender.cc',
+    url='http://github.com/codebendercc/seleniumTests',
+    packages=[],
+)
