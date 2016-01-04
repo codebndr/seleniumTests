6cbde740dfdc846ce47d0de3f8f25233ac792aa9
Christos Nianiakas <cnianiak@gmail.com>
2016-01-04 23:31:20 +0200

    Removed env variable from examples test.
    Will use the value from env_vars.sh.

--- bin/test_examples.sh
+++ bin/test_examples.sh
@@ -2,7 +2,6 @@
 
 source ./env_vars.sh
 export CAPABILITIES='capabilities_firefox.yaml'
-export CODEBENDER_SELENIUM_HUB_URL="http://127.0.0.1:4444/wd/hub"
 cd ..
 time tox tests/libraries -- --url=https://codebender.cc --source=codebender_cc -F
 RETVAL=$?
