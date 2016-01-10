2ddea6fcb117b89aec9ac255ef3b26ec5b0b1cfc
Christos Nianiakas <cnianiak@gmail.com>
2016-01-10 16:21:32 +0200

    Running in Firefox only the common tests.
    Until a method for changing the UA in Chrome is implemented.

--- bin/test_common.sh
+++ bin/test_common.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source ./env_vars.sh
+export CAPABILITIES='capabilities_firefox.yaml'
 cd ..
 time tox tests/common -- --url=https://codebender.cc --source=codebender_cc
 RETVAL=$?
