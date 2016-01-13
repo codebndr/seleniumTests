2a9f468b395e9f4c550e8463f6bd9995faf68d32
Christos Nianiakas <cnianiak@gmail.com>
2016-01-13 18:56:01 +0200

    Corrected test_common.sh
    Returning the correct return value from tests.
    Running each test on a separate command in order to have more control on which tests run each time.

--- bin/test_common.sh
+++ bin/test_common.sh
@@ -3,15 +3,29 @@
 source ./env_vars.sh
 export CAPABILITIES='capabilities_firefox.yaml'
 cd ..
-export SELENIUM_USER_AGENT='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0 codebender-selenium'
-time tox tests/common/walkthrough -- --url=https://codebender.cc --source=codebender_cc
-RETVAL=$?
-export SELENIUM_USER_AGENT='Mozilla/5.0 (Windows NT 6.1; rv:43.0) Gecko/20100101 Firefox/43.0 codebender-selenium'
-time tox tests/common/walkthrough -- --url=https://codebender.cc --source=codebender_cc
-RETVAL=$?
-export SELENIUM_USER_AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1; rv:43.0) Gecko/20100101 Firefox/43.0 codebender-selenium'
-time tox tests/common/walkthrough -- --url=https://codebender.cc --source=codebender_cc
-RETVAL=$?
+
+RETVALS=()
+time tox tests/common/embedded_views -- --url=https://codebender.cc --source=codebender_cc
+
+RETVALS+=($?)
+time tox tests/common/home -- --url=https://codebender.cc --source=codebender_cc
+RETVALS+=($?)
+
+time tox tests/common/sketch -- --url=https://codebender.cc --source=codebender_cc
+RETVALS+=($?)
+
+time tox tests/common/user_home -- --url=https://codebender.cc --source=codebender_cc
+RETVALS+=($?)
+
 cd -
+
+RETVAL=0
+for i in "${RETVALS[@]}"
+do
+    if [ ${i} -ne 0 ]; then
+        RETVAL=${i}
+    fi
+done
+
 echo "tests return value: ${RETVAL}"
 exit ${RETVAL}
