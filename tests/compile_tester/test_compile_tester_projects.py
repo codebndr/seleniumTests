f7454a94daf3f38d65b901f9aeb001174ac51199
Konstantina Kastanara <kastanarakonstantina@gmail.com>
2016-01-22 17:46:07 +0200

    Corrected comment to test_compile_tester_projects.py file.

--- tests/compile_tester/test_compile_tester_projects.py
+++ tests/compile_tester/test_compile_tester_projects.py
@@ -14,7 +14,7 @@ class TestCompileTester(SeleniumTestCase):
     # does not exist anywhere else.
     @pytest.mark.requires_url(LIVE_SITE_URL)
     def test_compile_all_user_projects(self):
-        """Tests that all library examples compile successfully."""
+        """Tests that all user's sketches compile successfully."""
         self.compile_all_sketches(COMPILE_TESTER_URL, '#user_projects tbody a',
             iframe=True, logfile=COMPILE_TESTER_LOGFILE,
             compile_type='sketch', create_report=True)
