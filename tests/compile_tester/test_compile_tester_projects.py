b015fee826a745fc5f4893a2c030fdd2192837f0
Konstantina Kastanara <kastanarakonstantina@gmail.com>
2016-01-22 17:49:23 +0200

    Removed unused code from test_compile_tester_projects.py related to bachelor's site.

--- tests/compile_tester/test_compile_tester_projects.py
+++ tests/compile_tester/test_compile_tester_projects.py
@@ -18,22 +18,3 @@ class TestCompileTester(SeleniumTestCase):
         self.compile_all_sketches(COMPILE_TESTER_URL, '#user_projects tbody a',
             iframe=True, logfile=COMPILE_TESTER_LOGFILE,
             compile_type='sketch', create_report=True)
-
-    # Here we require the bachelor site since cb_compile_tester's projects are
-    # already uploaded to the live site.
-    @pytest.mark.requires_source(SOURCE_BACHELOR)
-    def test_compile_local_files(self, tester_login):
-        """Tests that we can upload all of cb_compile_tester's projects
-        (stored locally in test_data/cb_compile_tester), compile them,
-        and finally delete them."""
-        upload_limit = None if self.run_full_compile_tests else 1
-
-        test_files = [os.path.join(COMPILE_TESTER_DIR, name)
-            for name in next(os.walk(COMPILE_TESTER_DIR))[2]][:upload_limit]
-        projects = [self.upload_project(fname) for fname in test_files]
-        project_names, project_urls = zip(*projects)
-
-        self.compile_sketches(project_urls, logfile=COMPILE_TESTER_LOGFILE,
-                              compile_type='sketch', create_report=True)
-        for name in project_names:
-            self.delete_project(name)
