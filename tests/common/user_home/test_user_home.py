bea5ae2ae07b3f788b186697db2c6165ca725841
konstantina <koka@codebender.cc>
2016-03-31 13:16:57 +0300

    Updated functions that test upload functionality. Due to changes in homepage, functions that test upload ino file and upload zip file had to be updated.

--- tests/common/user_home/test_user_home.py
+++ tests/common/user_home/test_user_home.py
@@ -4,7 +4,8 @@ import pytest
 from codebender_testing.config import TEST_DATA_INO
 from codebender_testing.config import TEST_DATA_ZIP
 from codebender_testing.utils import SeleniumTestCase
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 # Name to be used for the new project that is created.
 NEW_PROJECT_NAME = 'selenium_TestUserHome'
@@ -23,20 +24,36 @@ class TestUserHome(SeleniumTestCase):
         default it is inferred from the file name.
         We delete the project if it is successfully uploaded.
         """
-        try:
-            upload_name = self.upload_project(dropzone_selector, test_fname, sketch_name)
-            selector = '#project_list li[data-name="'+ str(upload_name.lower()) +'"]'
-            project_uploaded = self.get_element(By.CSS_SELECTOR, selector).text.split('\n')[0]
-            assert upload_name == project_uploaded
-        finally:
-            self.delete_project(upload_name)
+        # Upload the file.
+        upload_name = self.upload_project(dropzone_selector, test_fname, sketch_name)
+        # Wait for the success mark to appear before closing the modal.
+        WebDriverWait(self.driver, 30).until(
+            expected_conditions.visibility_of_element_located(
+                (By.CSS_SELECTOR, ".dz-success-mark")
+            )
+        )
+        close_btn = self.get_element(By.CSS_SELECTOR, "#home-upload-sketch-modal .btn-danger")
+        close_btn.click()
+        # Check that the uploaded file appears on the homepage.
+        selector = '#project_list li[data-name="'+ upload_name +'"]'
+        project_uploaded = self.get_element(By.CSS_SELECTOR, selector).text.split('\n')[0]
+        assert upload_name == project_uploaded
+        self.delete_project(upload_name)
 
     def test_upload_project_ino(self):
         """Tests that we can upload a .ino file."""
-        self._upload_test('#uploadInoModal form', TEST_DATA_INO)
+        upload_button = self.get_element(By.CSS_SELECTOR, '#sketch-upload-button')
+        upload_button.click()
+        sketch_upload_ino = self.get_element(By.CSS_SELECTOR, '#upload-sketch-ino')
+        sketch_upload_ino.click()
+        self._upload_test('#dropzoneForm', TEST_DATA_INO)
 
     def test_upload_project_zip(self):
         """Tests that we can successfully upload a zipped project."""
         # TODO: how is the project name inferred from the zip file?
         # Hardcoding the contents of the zip file feels weird here.
-        self._upload_test('#uploadFolderZip form', TEST_DATA_ZIP, sketch_name='upload_zip')
+        upload_button = self.get_element(By.CSS_SELECTOR, '#sketch-upload-button')
+        upload_button.click()
+        sketch_upload_zip = self.get_element(By.CSS_SELECTOR, '#upload-sketch-zip')
+        sketch_upload_zip.click()
+        self._upload_test('#dropzoneForm', TEST_DATA_ZIP, sketch_name='upload_zip')
