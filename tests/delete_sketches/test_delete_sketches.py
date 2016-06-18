4e4097487b93be95e348fcf3d44b94a147946157
Christos Nianiakas <cnianiak@gmail.com>
2016-06-18 16:29:28 +0300

    Allowing delete sketch test to run in all users. Added delete sketch test in try-except block to handle the case where no sketches are present on the home page.

--- tests/delete_sketches/test_delete_sketches.py
+++ tests/delete_sketches/test_delete_sketches.py
@@ -1,15 +1,17 @@
 from codebender_testing.config import STAGING_SITE_URL
 from selenium.webdriver.common.by import By
 from codebender_testing.utils import SeleniumTestCase
-import pytest
+
 
 class TestDeleteAllSketches(SeleniumTestCase):
 
-    @pytest.mark.requires_url(STAGING_SITE_URL)
     def test_delete(self, tester_login):
-        sketches = self.find_all('#project_list>li .sketch-block-title >a')
-        projects = []
-        for sketch in sketches:
-            projects.append(sketch.text)
-        for project in projects:
-            self.delete_project(project)
+        try:
+            sketches = self.find_all('#project_list > li .sketch-block-title > a')
+            projects = []
+            for sketch in sketches:
+                projects.append(sketch.text)
+            for project in projects:
+                self.delete_project(project)
+        except:
+            print 'No sketches found'
