8ab2badf3438e6ef855f1d1d75ad2b543e9c1725
Penelope Zisimopoulou <popi@codebender.cc>
2016-06-27 15:22:41 +0300

    Fix any issues

--- tests/common/sketches_counters/test_sketches_counters.py
+++ tests/common/sketches_counters/test_sketches_counters.py
@@ -3,78 +3,76 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from codebender_testing import config
+from codebender_testing.config import STAGING_SITE_URL
+import os
+import time
+import pytest
 
 class TestSketchesCounters(SeleniumTestCase):
-    
     def test_sketches_counters(self):
-	self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://staging.codebender.cc/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
         driver = self.driver
-        driver.get(self.base_url + "/")
+        self.open("/")
+
+        #Login and visit the new home page.
+        credentials = {
+        'username': os.environ.get('CODEBENDER_TEST_USER'),
+          'password': os.environ.get('CODEBENDER_TEST_PASS'),
+        }
         driver.find_element_by_id("login_btn").click()
         driver.find_element_by_id("username").clear()
-        driver.find_element_by_id("username").send_keys("demo_user")        
-	driver.find_element_by_id("password").clear()
-        driver.find_element_by_id("password").send_keys("testerPASS")
+        driver.find_element_by_id("username").send_keys(credentials['username'])
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys(credentials['password'])
         driver.find_element_by_id("_submit").click()
+
+        #check that there are not any sketches.
+        assert driver.find_element_by_id("private-sketches-counter").text == "0"
+        assert driver.find_element_by_id("public-sketches-counter").text == "0"
+
+        #Create 2 public sketches and Create 2 private sketches
         driver.find_element_by_id("create_sketch_btn").click()
-
-
         driver.find_element_by_id("create-sketch-modal-action-button").click()
-	time.sleep(5)
+        self.get_element(By.ID, "save")
         driver.find_element_by_id("logo_small").click()
         driver.find_element_by_id("create_sketch_btn").click()
         driver.find_element_by_id("create-sketch-modal-action-button").click()
-	time.sleep(5)        
-	driver.find_element_by_id("logo_small").click()
+        self.get_element(By.ID, "save")
+        driver.find_element_by_id("logo_small").click()
         driver.find_element_by_id("create_sketch_btn").click()
         driver.find_element_by_xpath("(//input[@name='create-sketch-modal-title'])[2]").click()
         driver.find_element_by_id("create-sketch-modal-action-button").click()
-        time.sleep(5)
-	driver.find_element_by_id("logo_small").click()
+        self.get_element(By.ID, "save")
+        driver.find_element_by_id("logo_small").click()
         driver.find_element_by_id("create_sketch_btn").click()
         driver.find_element_by_xpath("(//input[@name='create-sketch-modal-title'])[2]").click()
         driver.find_element_by_id("create-sketch-modal-action-button").click()
-        time.sleep(5)
-	driver.find_element_by_id("logo_small").click()
-	assert driver.find_element_by_id("private-sketches-counter").text=="2"
-	assert driver.find_element_by_id("public-sketches-counter").text=="2"        
-	assert driver.find_element_by_id("privateProjectAvailableNumber").text=="0"	        
-	assert driver.find_element_by_id("available-private-projects-counter").text=="0/2"
-	assert "Add more"==driver.find_element_by_link_text("Add more").text
+        self.get_element(By.ID, "save")
+        driver.find_element_by_id("logo_small").click()
 
-	#Private-->Public
-        driver.find_element_by_link_text("Edit Info").click()
-	time.sleep(5)
-        driver.find_element_by_name("edit-sketch-modal-title").click()
-        driver.find_element_by_id("edit-sketch-modal-action-button").click()
-	time.sleep(5)
-	assert driver.find_element_by_id("private-sketches-counter").text=="1"
-	assert driver.find_element_by_id("public-sketches-counter").text=="3" 
+        #Check that the counters have the correct value.
+        assert driver.find_element_by_id("private-sketches-counter").text == "2"
+        assert driver.find_element_by_id("public-sketches-counter").text == "2"
 
-	#Public-->Private
-        driver.find_element_by_link_text("Edit Info").click()
-	time.sleep(5)
-        driver.find_element_by_xpath("(//input[@name='edit-sketch-modal-title'])[2]").click()
-        driver.find_element_by_id("edit-sketch-modal-action-button").click()
-     	time.sleep(5)
-        assert driver.find_element_by_id("private-sketches-counter").text=="2"
-	assert driver.find_element_by_id("public-sketches-counter").text=="2" 
-	#for x in range(0, 3):
-	#	driver.find_element_by_css_selector("i.fa.fa-trash").click()
-	#	driver.find_element_by_css_selector("div.modal-footer.delete-sketch-modal-footer > button.btn.delete-sketch-modal-button").click()
-		#driver.find_element_by_css_selector("div.modal-footer.delete-sketch-modal-footer > button.btn.btn-danger").click()
+        #Check that the counter for private projects also appears at the Badges section and has the  correct value.
+        assert driver.find_element_by_id("privateProjectAvailableNumber").text == "0"
+        assert driver.find_element_by_id("available-private-projects-counter").text == "0/2"
 
+        #Check that if your account has private projects (e.g. 2) and all yor projects are public,counter for available private sketches should be 0/number of total private sketches(e.g.0/2)         and the link to the private sketches should be Add more.
+        assert "Add more" == driver.find_element_by_link_text("Add more").text
 
+class TestDeleteAllSketches(SeleniumTestCase):
 
-	driver.find_element_by_id("logout").click()
-    
- 
-
-
+    @pytest.mark.requires_url(STAGING_SITE_URL)
+    def test_delete(self, tester_login):
+        try:
+            sketches = self.find_all('#project_list > li .sketch-block-title > a')
+            projects = []
+            for sketch in sketches:
+                projects.append(sketch.text)
+            for project in projects:
+                self.delete_project(project)
+        except:
+            print 'No sketches found'
+        self.logout()
