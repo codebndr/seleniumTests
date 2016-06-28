a9fdec2ffb456f78dd0743496f27ac72c0bcb065
Penelope Zisimopoulou <popi@codebender.cc>
2016-06-28 14:56:58 +0300

    Updated script-Added new steps

--- tests/common/sketches_counters/test_sketches_counters.py
+++ tests/common/sketches_counters/test_sketches_counters.py
@@ -5,20 +5,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from codebender_testing import config
 from codebender_testing.config import STAGING_SITE_URL
+from selenium.webdriver.common.action_chains import ActionChains
 import os
 import time
 import pytest
 
 class TestSketchesCounters(SeleniumTestCase):
+
     def test_sketches_counters(self):
         self.driver.implicitly_wait(30)
         driver = self.driver
         self.open("/")
-
         #Login and visit the new home page.
         credentials = {
-        'username': os.environ.get('CODEBENDER_TEST_USER'),
-          'password': os.environ.get('CODEBENDER_TEST_PASS'),
+            'username': os.environ.get('CODEBENDER_TEST_USER'),
+            'password': os.environ.get('CODEBENDER_TEST_PASS'),
         }
         driver.find_element_by_id("login_btn").click()
         driver.find_element_by_id("username").clear()
@@ -27,9 +28,8 @@ class TestSketchesCounters(SeleniumTestCase):
         driver.find_element_by_id("password").send_keys(credentials['password'])
         driver.find_element_by_id("_submit").click()
 
-        #check that there are not any sketches.
-        assert driver.find_element_by_id("private-sketches-counter").text == "0"
-        assert driver.find_element_by_id("public-sketches-counter").text == "0"
+        assert driver.find_element_by_id("private-sketches-counter").text=="0"
+        assert driver.find_element_by_id("public-sketches-counter").text=="0"
 
         #Create 2 public sketches and Create 2 private sketches
         driver.find_element_by_id("create_sketch_btn").click()
@@ -52,15 +52,97 @@ class TestSketchesCounters(SeleniumTestCase):
         driver.find_element_by_id("logo_small").click()
 
         #Check that the counters have the correct value.
-        assert driver.find_element_by_id("private-sketches-counter").text == "2"
-        assert driver.find_element_by_id("public-sketches-counter").text == "2"
+        assert driver.find_element_by_id("private-sketches-counter").text=="2"
+        assert driver.find_element_by_id("public-sketches-counter").text=="2"
 
-        #Check that the counter for private projects also appears at the Badges section and has the  correct value.
-        assert driver.find_element_by_id("privateProjectAvailableNumber").text == "0"
-        assert driver.find_element_by_id("available-private-projects-counter").text == "0/2"
+        #Check that the counter for private projects also appears at the Badges section and has the correct value.
+        assert driver.find_element_by_id("privateProjectAvailableNumber").text=="0"
+        assert driver.find_element_by_id("available-private-projects-counter").text=="0/2"
 
-        #Check that if your account has private projects (e.g. 2) and all yor projects are public,counter for available private sketches should be 0/number of total private sketches(e.g.0/2)         and the link to the private sketches should be Add more.
+        #Check that if your account has private projects (e.g. 2) and all yor projects are public, counter for available private sketches should be 0/number of total private sketches(e.g. 		0/2) and the link to the private sketches should be Add more.
         assert "Add more" == driver.find_element_by_link_text("Add more").text
+
+        #Check that All, Public and Private buttons work, clicking on each of them and verifying
+        #that the correct number of sketches is viewed each time
+        #Check "All button"
+        self.get_element(By.XPATH, "//div[@id='filter-options']/label[2]").click()
+        sketches = self.find_all('#project_list >li')
+        assert len(sketches) == 4
+        #Check "Public button"
+        self.get_element(By.XPATH, "//div[@id='filter-options']/label[2]").click()
+        sketches = self.find_all('#project_list >li .cb-icon-globe-inv')
+        assert len(sketches) == 2
+        #Check "Private button"
+        self.get_element(By.XPATH, "//div[@id='filter-options']/label[3]").click()
+        sketches = self.find_all('#project_list >li .fa-lock')
+        assert len(sketches) == 2
+        self.get_element(By.XPATH, "//div[@id='filter-options']/label[2]").click()
+
+        #ToDo Check that the number of private projects available is updated each time that you make a change (e.g from private to public and vice versa).
+
+        #Check that the counter for available private sketches also appears at the upload sketch modal (ino, zip, multiple zip) and at the create and edit sketch modal.
+        self.get_element(By.ID, "sketch-upload-button").click()
+        self.get_element(By.ID, "upload-sketch-ino").click()
+        assert self.get_element(By.ID, "upload-sketch-modal-available-private-projects").text == "0/2"
+        driver.find_element_by_xpath("//div[4]/div[3]/button").click()
+
+        self.get_element(By.ID, "sketch-upload-button").click()
+        self.get_element(By.ID, "upload-sketch-zip").click()
+        assert self.get_element(By.ID, "upload-sketch-modal-available-private-projects").text == "0/2"
+        driver.find_element_by_xpath("//div[4]/div[3]/button").click()
+
+        self.get_element(By.ID, "sketch-upload-button").click()
+        self.get_element(By.ID, "upload-sketch-folder-zip").click()
+        assert self.get_element(By.ID, "upload-sketch-modal-available-private-projects").text == "0/2"
+        driver.find_element_by_xpath("//div[4]/div[3]/button").click()
+
+        self.get_element(By.ID, "create_sketch_btn").click()
+        assert self.get_element(By.ID, "create-sketch-modal-available-private-projects").text == "0/2"
+        driver.find_element_by_xpath("//button[2]").click()
+
+        #Open the create sketch modal and try to create a new project without providing a name.
+        #The Create button should be disabled.
+        self.get_element(By.ID, "create_sketch_btn").click()
+        self.get_element(By.ID, "create-sketch-name").clear()
+        self.get_element(By.ID, "create-sketch-name").send_keys(Keys.CONTROL + "a")
+        self.get_element(By.ID, "create-sketch-name").send_keys(Keys.DELETE)
+        assert not self.get_element(By.ID, "create-sketch-modal-action-button").is_enabled()
+        driver.find_element_by_xpath("//button[2]").click()
+
+
+        #Check that sketch name is auto-generated as: Untitled Sketch CURRENT_DATE.
+        self.get_element(By.ID, "create_sketch_btn").click()
+        current_date_text= "Untitled Sketch " + time.strftime("%Y-%m-%d")
+        assert self.get_element(By.ID, "create-sketch-name").get_attribute("value") == current_date_text
+        driver.find_element_by_xpath("//button[2]").click()
+
+        #Check that when you create the sketch you are redirected into the editor.
+        self.get_element(By.ID, "create_sketch_btn").click()
+        self.get_element(By.ID, "create-sketch-modal-action-button").click()
+        self.get_element(By.ID, "cb_cf_flash_btn")
+        self.get_element(By.ID, "logo_small").click()
+
+        #Check that short description
+        #Write short description with 17 characters
+        self.get_element(By.ID, "create_sketch_btn").click()
+        self.get_element(By.ID, "create-sketch-modal-sort-description").click()
+        self.get_element(By.ID, "create-sketch-modal-sort-description").send_keys("short description")
+        assert self.get_element(By.ID, "create-sketch-modal-sort-description-count").text == "123"
+        driver.find_element_by_xpath("//button[2]").click()
+
+        #Write short description with 140 characters
+        self.get_element(By.ID, "create_sketch_btn").click()
+        self.get_element(By.ID, "create-sketch-modal-sort-description").click()
+        self.get_element(By.ID, "create-sketch-modal-sort-description").send_keys("shortdescriptionshortdescriptionshortdescriptionshortdescriptionshortdescriptionshortdescriptionshortdescriptionshortdescriptionshortdescrip")
+        assert self.get_element(By.ID, "create-sketch-modal-sort-description-count").text == "0"
+        driver.find_element_by_xpath("//button[2]").click()
+
+        #Write short description with 153 characters
+        self.get_element(By.ID, "create_sketch_btn").click()
+        self.get_element(By.ID, "create-sketch-modal-sort-description").click()
+        self.get_element(By.ID, "create-sketch-modal-sort-description").send_keys("short descriptionshort descriptionshort descriptionshort descriptionshort descriptionshort descriptionshort descriptionshort descriptionshort description")
+        assert self.get_element(By.ID, "create-sketch-modal-sort-description-count").text == "-13"
+        driver.find_element_by_xpath("//button[2]").click()
 
 class TestDeleteAllSketches(SeleniumTestCase):
 
@@ -75,4 +157,28 @@ class TestDeleteAllSketches(SeleniumTestCase):
                 self.delete_project(project)
         except:
             print 'No sketches found'
+
+        #Create Clone
+        self.get_element(By.ID,"create_sketch_btn").click()
+        self.get_element(By.ID,"create-sketch-modal-action-button").click()
+        self.get_element(By.ID, "save")
+        self.get_element(By.ID,"logo_small").click()
+        self.get_element(By.XPATH, "//div[4]/a[2]/i").click()
+        self.get_element(By.ID, "save")
+        self.get_element(By.ID,"logo_small").click()
+
+class TestDeleteAllSketches(SeleniumTestCase):
+
+    @pytest.mark.requires_url(STAGING_SITE_URL)
+    def test_delete(self, tester_login):
+        try:
+            sketches = self.find_all('#project_list > li .sketch-block-title > a')
+            projects = []
+            for sketch in sketches:
+                projects.append(sketch.text)
+            for project in projects:
+                self.delete_project(project)
+        except:
+            print 'No sketches found'
+
         self.logout()
